2b3e1a3638fa8bdb6e7f26c471b9ded7e19fbfe9
SoloAlphus <106820883+SoloAlphus@users.noreply.github.com>
2022-06-03 19:36:04 +0530

    Create finditout.py

---
+++ finditout.py
@@ -0,0 +1,11 @@
+str1=108
+str2=101
+str3=116
+str4=116
+str5=101
+str6=114
+
+str=chr(str1)+chr(str2)+chr(str3)+chr(str4)+chr(str5)+chr(str6)
+print("The link is:",str,123,".netlify.app")
+
+#oops sorry I didn't mention chr() function
